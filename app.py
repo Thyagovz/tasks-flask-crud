2359fc1a4b43907bd19342f90230715b059861ed
Thyago Euclides <flooddk@gmail.com>
2025-11-01 21:37:44 -0400

    Sets up initial Flask app and Task model
    Establishes the foundational structure for a task management API.
    
    Introduces the `Task` model, defining its attributes and a method for converting objects to dictionaries. Sets up the main Flask application and a basic `/tasks` POST endpoint, providing a starting point for creating new tasks.

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+from models.task import Task
+
+app = Flask(__name__)
+
+
+# CRUD
+# Create, Read, Update and Delete = Criar, Ler, Atualizar e Deletar
+# Tabela: Tarefa
+
+tasks = []
+
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    data = request.get_json()
+    print(data)
+    return "Test"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
