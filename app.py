f746fbeb5e99454e9699879cdc3e88d2a70eb180
Thyago Euclides <flooddk@gmail.com>
2025-11-02 15:21:34 -0400

    Adds endpoint to list all tasks
    Introduces a new GET /tasks endpoint to retrieve all available tasks.
    
    Serializes existing task objects into a list of dictionaries for JSON output, enabling clients to fetch the current task list.

--- app.py
+++ app.py
@@ -24,5 +24,16 @@ def create_task():
     return jsonify({"message": "Nova tarefa criada com sucesso"})
 
 
+@app.route("/tasks", methods=["GET"])
+def get_tasks():
+    task_list = [task.to_dict() for task in tasks]
+
+    output = {
+        "tasks": task_list,
+        "total_taks": 0
+    }
+    return jsonify(output)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
